e3151233c3a960a571dd3e1d0338121971524a92
Zhalgas Baibolov <zhalgasbaibolov@.com>
2021-07-19 16:27:51 +0600

    deleted login

--- pages/index.vue
+++ pages/index.vue
@@ -2,19 +2,8 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <v-card class="logo py-4 d-flex justify-center">
-        <login />
       </v-card>
-      <v-card>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+      
     </v-col>
   </v-row>
 </template>
