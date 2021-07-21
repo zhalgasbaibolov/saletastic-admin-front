4282c35b9bbdc07442eb19e7ec56851debc688e3
karnurmax <karnurmax@mail.ru>
2021-07-21 16:35:42 +0600

    memberstack plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/memberstack.js'
+    '~/plugins/memberstack.client.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
--- plugins/memberstack.js
+++
@@ -1,4 +0,0 @@
-const MemberStack = window.MemberStack;
-MemberStack.onReady.then(function(member) {   
- window.member = member;
-})
