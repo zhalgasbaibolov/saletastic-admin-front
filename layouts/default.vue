d9c542006eddf1c4b378d283ec2131a0ede4d528
karnurmax <karnurmax@mail.ru>
2021-07-21 16:01:49 +0600

    fix hide/show rules

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,71 +8,68 @@
       app
     >
       <v-list>
-        <template data-ms-content="!members">
-          <v-list-item>
-            <v-list-item-action>
-              <v-icon>mdi-apps</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-btn
-                color="primary"
-                data-ms-membership="60f7b76fc2135000047acf81"
-              >
-                sign up
-              </v-btn>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item to="#/ms/login">
-            <v-list-item-action>
-              <v-icon>mdi-apps</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title v-text="`Sign In`" />
-            </v-list-item-content>
-          </v-list-item>
-        </template>
-        <template data-ms-content="members">
-          <v-list-item to="#">
-            <v-list-item-action>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <h3 data-ms-member="email"></h3>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item to="#/ms/profile">
-            <v-list-item-action>
-              <v-icon>mdi-apps</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title v-text="`Profile`" />
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item to="settings">
-            <v-list-item-action>
-              <v-icon>mdi-settings</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title v-text="`Profile`" />
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item to="#/ms/logout">
-            <v-list-item-action>
-              <v-icon>mdi-apps</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title v-text="`Sign Out`" />
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item to="/chat">
-            <v-list-item-action>
-              <v-icon>mdi-chat</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title v-text="`Chat`" />
-            </v-list-item-content>
-          </v-list-item>
-        </template>
+        <v-list-item data-ms-content="!members">
+          <v-list-item-action>
+            <v-icon>mdi-apps</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-btn
+              color="primary"
+              data-ms-membership="60f7b76fc2135000047acf81"
+            >
+              sign up
+            </v-btn>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item to="#/ms/login" data-ms-content="!members">
+          <v-list-item-action>
+            <v-icon>mdi-apps</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="`Sign In`" />
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item to="#" data-ms-content="members">
+          <v-list-item-action>
+            <v-icon>mdi-account</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <h3 data-ms-member="email"></h3>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item to="#/ms/profile" data-ms-content="members">
+          <v-list-item-action>
+            <v-icon>mdi-apps</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="`Profile`" />
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item to="settings" data-ms-content="members">
+          <v-list-item-action>
+            <v-icon>mdi-settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="`Profile`" />
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item to="#/ms/logout" data-ms-content="members">
+          <v-list-item-action>
+            <v-icon>mdi-apps</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="`Sign Out`" />
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item to="/chat" data-ms-content="members">
+          <v-list-item-action>
+            <v-icon>mdi-chat</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="`Chat`" />
+          </v-list-item-content>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
     <v-app-bar :clipped-left="false" fixed app>
