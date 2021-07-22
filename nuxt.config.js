bf7cf4d17a59574f7ae683fc5b52be0231712010
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-07-22 16:58:51 +0600

    added socket plugin and deleted old login form

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/memberstack.client.js'
+    '~/plugins/memberstack.client.js',
+    '~/plugins/socket.client.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
