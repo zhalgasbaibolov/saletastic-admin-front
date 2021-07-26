20437fb588379853fec05393eb0fb05b9537371c
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-07-26 12:33:56 +0600

    working version between pages

--- pages/chat.vue
+++ pages/chat.vue
@@ -56,37 +56,32 @@
                     john doe
                   </v-card-title>
                   <v-divider></v-divider>
-                  <v-card-text class="flex-grow-1 overflow-y-auto">
-                    <v-menu offset-y>
-                      <template #activator="{ on }">
-                        <ul v-for="(msg, index) in messages" :key="index" class="d-flex flex-row-reverse">
-                             <v-card-text
-                                class="pa-1 mb-1"
-                                v-on="on"
-                              >
-                               {{ msg.text }}
-                                <sub
-                                  class="ml-2"
-                                  style="font-size: 0.5rem;"
-                                >{{ msg.date.split('T')[1].slice(0, -5) }}</sub>
-                              </v-card-text>
-                        </ul>
-                      </template>
-                    </v-menu>
-                  </v-card-text>
-                  <v-card-text>
-                      <v-text-field 
+                  <v-card height="450">
+                        <div>
+                          <ul class="pages">
+                            <li class="chat page">
+                              <div class="chatArea">
+                                <ul ref="messages" class="messages">
+                                  <li v-for="(msg, index) in messages" :key="index" class="message">
+                                    <i :title="msg.date">
+                                      {{ msg.date.split('T')[1].slice(0, -2) }}
+                                    </i>: {{ msg.text }}
+                                  </li>
+                                </ul>
+                              </div>
+                            </li>
+                          </ul>
+                        </div>
+                    </v-card>
+                      <input
                               v-model="message"
-                              placeholder="type here..."
-                              @click:append="toggleMarker"
+                              class="inputMessage" 
+                              type="text" 
                               @keyup.enter="sendMessage"
-                              @click:append-outer="sendMessage"
-                              append-outer-icon="mdi-send"
+                              placeholder="Type here..."
                               filled
                               outlined
-                              type="text"
-                            />
-                  </v-card-text>
+                            >
                 </v-card>
               </v-responsive>
             </v-col>
@@ -106,12 +101,7 @@ export default {
   },
   data () {
     return {
-        password: 'Password',
-        show: false,
         message: '',
-        marker: true,
-        iconIndex: 0,
-        selectedItem: 1,
         activeChat: 1,
         parents: [
       {
@@ -132,14 +122,14 @@ export default {
     ],
     }
   },
-  // watch: {
-  //   messages: ''
-  // },
   
   computed: {
     icon() {
       return this.icons[this.iconIndex]
     },
+  },
+  watch: {
+    messages: 'scrollToBottom'
   },
   beforeMount () {
     socket.on('new-message', (message) => {
@@ -148,16 +138,11 @@ export default {
   },
   
   mounted () {
-    // this.scrollToBottom()
+    this.scrollToBottom()
   },
 
   methods: {
-    toggleMarker() {
-      this.marker = !this.marker
-    },
     sendMessage() {
-      this.resetIcon()
-      this.clearMessage()
       if (!this.message.trim()) { return }
       const message = {
         date: new Date().toJSON(),
@@ -167,17 +152,88 @@ export default {
       this.message = ''
       socket.emit('send-message', message)
     },
-    // scrollToBottom () {
-    //   this.$nextTick(() => {
-    //     this.$refs.messages.scrollTop = this.$refs.messages.scrollHeight
-    //   })
-    // },
-    clearMessage() {
-      this.message = ''
-    },
-    resetIcon() {
-      this.iconIndex = 0
+    scrollToBottom () {
+      this.$nextTick(() => {
+        this.$refs.messages.scrollTop = this.$refs.messages.scrollHeight
+      })
     },
   },
 }
 </script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-weight: 300;
+  -webkit-font-smoothing: antialiased;
+}
+html, input {
+  font-family:
+    "HelveticaNeue-Light",
+    "Helvetica Neue Light",
+    "Helvetica Neue",
+    Helvetica,
+    Arial,
+    "Lucida Grande",
+    sans-serif;
+}
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+ul {
+  list-style: none;
+  word-wrap: break-word;
+}
+ul li { padding: 0.5rem 1rem; }
+/* Pages */
+.pages {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+}
+.page {
+  height: 100%;
+  position: absolute;
+  width: 100%;
+}
+/* Font */
+.messages {
+  font-size: 100%;
+}
+.inputMessage {
+  font-size: 100%;
+}
+.log {
+  color: gray;
+  font-size: 70%;
+  margin: 5px;
+  text-align: center;
+}
+/* Messages */
+.chatArea {
+  height: 100%;
+  padding-bottom: 60px;
+}
+.messages {
+  height: 100%;
+  margin: 0;
+  overflow-y: scroll;
+  padding: 10px 20px 10px 20px;
+}
+.inputMessage {
+  border: 10px solid #3B8070;
+  bottom: 0;
+  height: 60px;
+  left: 0;
+  outline: none;
+  padding-left: 10px;
+  position: absolute;
+  right: 0;
+  width: 100%;
+}
+</style>
