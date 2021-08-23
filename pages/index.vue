7285aa4ddfa62c019dd4a9cf7c46ce61a7e01eeb
karnurmax <karnurmax@mail.ru>
2021-08-23 14:56:01 +0600

    run eslint

--- pages/index.vue
+++ pages/index.vue
@@ -2,28 +2,15 @@
   <div id="app">
     <v-app id="inspire">
       <v-card color="basil">
-    
-        <v-tabs
-          v-model="tab"
-          background-color="transparent"
-          color="basil"
-          grow
-        >
-          <v-tab
-            v-for="item in items"
-            :key="item"
-          >
+        <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
+          <v-tab v-for="item in items" :key="item">
             {{ item }}
           </v-tab>
         </v-tabs>
-    
+
         <v-tabs-items v-model="tab">
           <v-tab-item>
-            <v-card
-              class="mx-auto text-center"
-              color="green"
-              dark
-            >
+            <v-card class="mx-auto text-center" color="green" dark>
               <v-card-text>
                 <v-sheet color="rgba(0, 0, 0, .12)">
                   <v-sparkline
@@ -34,26 +21,18 @@
                     stroke-linecap="round"
                     smooth
                   >
-                    <template #label="item">
-                      ${{ item.value }}
-                    </template>
+                    <template #label="item"> ${{ item.value }} </template>
                   </v-sparkline>
                 </v-sheet>
               </v-card-text>
 
               <v-card-text>
-                <div class="text-h4 font-weight-thin">
-                  Sales Last 24h
-                </div>
+                <div class="text-h4 font-weight-thin">Sales Last 24h</div>
               </v-card-text>
             </v-card>
           </v-tab-item>
           <v-tab-item>
-            <v-card
-              class="mx-auto text-center"
-              color="blue"
-              dark
-            >
+            <v-card class="mx-auto text-center" color="blue" dark>
               <v-card-text>
                 <v-sheet color="rgba(0, 0, 0, .12)">
                   <v-sparkline
@@ -64,17 +43,13 @@
                     stroke-linecap="round"
                     smooth
                   >
-                    <template #label="item">
-                      ${{ item.value }}
-                    </template>
+                    <template #label="item"> ${{ item.value }} </template>
                   </v-sparkline>
                 </v-sheet>
               </v-card-text>
 
               <v-card-text>
-                <div class="text-h4 font-weight-thin">
-                  Sales Last 7 days
-                </div>
+                <div class="text-h4 font-weight-thin">Sales Last 7 days</div>
               </v-card-text>
             </v-card>
           </v-tab-item>
@@ -86,35 +61,30 @@
                   :key="item.header"
                   v-text="item.header"
                 ></v-subheader>
-        
+
                 <v-divider
                   v-else-if="item.divider"
                   :key="index"
                   :inset="item.inset"
                 ></v-divider>
-        
-                <v-list-item
-                  v-else
-                  :key="item.title"
-                >
+
+                <v-list-item v-else :key="item.title">
                   <v-list-item-avatar>
                     <v-img :src="item.avatar"></v-img>
                   </v-list-item-avatar>
-        
+
                   <v-list-item-content>
                     <v-list-item-title v-html="item.title"></v-list-item-title>
-                    <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+                    <v-list-item-subtitle
+                      v-html="item.subtitle"
+                    ></v-list-item-subtitle>
                   </v-list-item-content>
                 </v-list-item>
               </template>
             </v-list>
           </v-tab-item>
           <v-tab-item>
-            <v-card
-              class="mx-auto text-center"
-              color="yellow"
-              dark
-            >
+            <v-card class="mx-auto text-center" color="yellow" dark>
               <v-card-text>
                 <v-sheet color="rgba(0, 0, 0, .12)">
                   <v-sparkline
@@ -125,17 +95,13 @@
                     stroke-linecap="round"
                     smooth
                   >
-                    <template #label="item">
-                      ${{ item.value }}
-                    </template>
+                    <template #label="item"> ${{ item.value }} </template>
                   </v-sparkline>
                 </v-sheet>
               </v-card-text>
 
               <v-card-text>
-                <div class="text-h4 font-weight-thin">
-                  Sales Last 24h
-                </div>
+                <div class="text-h4 font-weight-thin">Sales Last 24h</div>
               </v-card-text>
             </v-card>
           </v-tab-item>
@@ -147,30 +113,17 @@
 
 <script>
 export default {
-   data: () => ({
-      tab: null,
-        items: [
-          'Daily Sales Report', 'Weekly Sales Report', 'Buyer Reviews', 'Last Orders',
-        ],
-      valueDaily: [
-        423,
-        446,
-        675,
-        510,
-        590,
-        610,
-        760,
-      ],
-      valueWeekly: [
-        123,
-        246,
-        675,
-        210,
-        590,
-        110,
-        760,
-      ],
-      reviews: [
+  data: () => ({
+    tab: null,
+    items: [
+      'Daily Sales Report',
+      'Weekly Sales Report',
+      'Buyer Reviews',
+      'Last Orders',
+    ],
+    valueDaily: [423, 446, 675, 510, 590, 610, 760],
+    valueWeekly: [123, 246, 675, 210, 590, 110, 760],
+    reviews: [
       {
         avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
         title: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
@@ -183,23 +136,25 @@ export default {
       { divider: true, inset: true },
       {
         avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
-        title: '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
+        title:
+          '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
       },
       { divider: true, inset: true },
       {
         avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
-        title: '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
+        title:
+          '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
       },
       { divider: true, inset: true },
       {
         avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
-        title: '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+        title:
+          '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
       },
     ],
-    }),
+  }),
 }
 </script>
 
 <style>
-
 </style>
