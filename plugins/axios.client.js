82000e41b002ef160281b6bf1de96fcac96e5a9f
karnurmax <karnurmax@mail.ru>
2021-08-23 16:35:49 +0600

    rmv c.log

--- plugins/axios.client.js
+++ plugins/axios.client.js
@@ -3,6 +3,5 @@
 export default function ({ $axios, redirect }) {
   $axios.onRequest(config => {
     config.headers['shopify-access-token'] = localStorage.getItem('shopify-access-token')
-    console.log(config)
   })
 }
