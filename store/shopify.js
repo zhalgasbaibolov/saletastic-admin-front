d1529feefbc7ea66f8c8aa49bfc3470f648c9890
karnurmax <karnurmax@mail.ru>
2021-08-24 17:41:15 +0600

    loadShopifyInfo

--- store/shopify.js
+++ store/shopify.js
@@ -16,11 +16,12 @@ export const state = () => ({
  }
  export const actions = {
    loadShopifyInfo({commit}){
-    this.$axios
-    .post('/api/shopify/getinfo')
-    .then((res) => {
-      commit('setShop', res.data.shop)
-    })
-    .catch((err) => console.log('/api/shopify/getinfo', err))
+    return this.$axios
+      .post('/api/shopify/getinfo')
+      .then((res) => {
+        console.log('/api/shopify/getinfo:',res.data)
+        commit('setShop', res.data.shop)
+      })
+      .catch((err) => console.log('/api/shopify/getinfo', err))
    }
  }
