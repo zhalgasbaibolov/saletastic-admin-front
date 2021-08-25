edc66bec9595864c0c6a39740fcbd005d722ed23
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-25 12:58:44 +0600

    changed cors proxy

--- pages/sendSms.vue
+++ pages/sendSms.vue
@@ -62,7 +62,7 @@ export default {
       this.loading = true;
       try {
         const  response = await axios.post(
-          "https://fierce-fjord-51833.herokuapp.com/https://saletasticdev.herokuapp.com/send/sms",
+          "https://rocky-shore-79636.herokuapp.com/https://saletasticdev.herokuapp.com/send/sms",
           this.message
         );
 
