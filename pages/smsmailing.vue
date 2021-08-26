b50471e7f5fb6e6454f74a0aac804fc90bf07b8b
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-26 10:11:05 +0600

    new function: add, edit, delete

--- pages/smsmailing.vue
+++ pages/smsmailing.vue
@@ -13,7 +13,101 @@
       class="elevation-1"
     fixed-header
       height="450px">
-      
+        <template v-slot:top>
+        <v-toolbar
+          flat
+        >
+          <v-toolbar-title>List of Phone Numbers</v-toolbar-title>
+          <v-divider
+            class="mx-4"
+            inset
+            vertical
+          ></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog
+            v-model="dialogNew"
+            max-width="500px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                color="primary"
+                dark
+                class="mb-2"
+                v-bind="attrs"
+                v-on="on"
+              >
+                Add phone number
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="text-h5">{{ formTitle }}</span>
+              </v-card-title>
+  
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col
+                      cols="12"
+                    >
+                      <v-text-field
+                        class = "ma-1"
+                        outlined
+                        v-model="editedItem.number"
+                        label="Phone number"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+  
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="close"
+                >
+                  Cancel
+                </v-btn>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="save"
+                >
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-card>
+              <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <template v-slot:[`item.actions`]="{ item }">
+        <v-icon
+          small
+          class="mr-2"
+          @click="editItem(item)"
+        >
+          mdi-pencil
+        </v-icon>
+        <v-icon
+          small
+          @click="deleteItem(item)"
+        >
+          mdi-delete
+        </v-icon>
+      </template>
     </v-data-table>
     <v-row class="pa-2">
       <v-col>
@@ -103,34 +197,52 @@ export default {
     return {
       message: "",
       loading: false,
-        dialog: false,
-        singleSelect: false,
+      dialogNew: false,
+      dialog: false,
+      
+      dialogDelete: false,
+      singleSelect: false,
       selected: [],
       headers: [
         {
-          text: 'Phone Numbers',
+          text: 'Phone Number',
           align: 'start',
           sortable: false,
           value: 'number',
         },
         { text: 'Data source', value: 'source' },
+        { text: 'Actions', value: 'actions', sortable: false },
       ],
-      phoneNumbers: [
-        {
-          number: '+77075002029',
-          source: 'from Whatsapp DB'
-        },
-        {
-          number: '+77078629827',
-          source: 'from Shopify DB'
-        },
-        {
-          number: '+77079481010',
-          source: 'from Shopify DB'
-        }
-      ],
+      phoneNumbers: [],
+    editedIndex: -1,
+    editedItem: {
+      number: '',
+      source: 'Added by User',
+    },
+    defaultItem: {
+      number: '',
+      source: 'Added by User',
+    },
     }
     },
+    computed: {
+    formTitle () {
+      return this.editedIndex === -1 ? 'New Phone Number' : 'Edit Phone Number'
+    },
+  },
+
+  watch: {
+    dialogNew (val) {
+      val || this.close()
+    },
+    dialogDelete (val) {
+      val || this.closeDelete()
+    },
+  },
+
+  created () {
+    this.initialize()
+  },
     methods: {
     sendMessage() {
       try {
@@ -150,7 +262,57 @@ export default {
       } catch (err) {
         console.log(err);
       }
-    }
+    },
+    initialize () {
+      this.phoneNumbers = [
+        {
+          number: '+77075002029',
+          source: 'Whatsapp DB',
+         },
+       ]
+    },
+
+    editItem (item) {
+      this.editedIndex = this.phoneNumbers.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialogNew = true
+    },
+
+    deleteItem (item) {
+      this.editedIndex = this.phoneNumbers.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialogDelete = true
+    },
+
+    deleteItemConfirm () {
+      this.phoneNumbers.splice(this.editedIndex, 1)
+      this.closeDelete()
+    },
+
+    close () {
+      this.dialogNew = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    closeDelete () {
+      this.dialogDelete = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    save () {
+      if (this.editedIndex > -1) {
+        Object.assign(this.phoneNumbers[this.editedIndex], this.editedItem)
+      } else {
+        this.phoneNumbers.push(this.editedItem)
+      }
+      this.close()
+    },
 }
 }
 </script>
